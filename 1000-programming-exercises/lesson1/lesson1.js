887e06c4fe091da5914a7a07f670eb9cf6ca5a39
binguyen94 <nhannguyen.tnnnnn@gmail.com>
2023-04-17 08:21:44 +0700

    up

---
+++ 1000-programming-exercises/lesson1/lesson1.js
@@ -0,0 +1,7 @@
+let n = 10; // Đặt giá trị của n là 10
+let sum = 0; // Khởi tạo biến sum để tính tổng
+
+for (let i = 1; i <= n; i++) {
+  sum += i;
+}
+document.write("Tổng của các số từ 1 đến " + n + " là: " + sum);
